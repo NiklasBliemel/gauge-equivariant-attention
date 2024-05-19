b084acc3ebad014932c8721f4d6cdb30bed6c70f
Niklas Bliemel <niklasbliemel@air-von-niklas.speedport.ip>
2024-05-19 12:32:18 +0200

    hpd_test_3

--- GmresAttentionTestHpd.py
+++ GmresAttentionTestHpd.py
@@ -5,18 +5,20 @@ from Preconditioner import transformer, ptc
 import pickle
 from time import perf_counter_ns
 
-d_wc = D_WC(M, GAUGE_FIELD_SMALL)
+d_wc = D_WC(M, GAUGE_FIELD)
+
 
 def safe_gmres(tra_name):
     
     preconditioner = transformer(tra_name)
 
-    b = torch.rand(NUM_OF_LATTICES, *LATTICE_SMALL, GAUGE_DOF, NON_GAUGE_DOF, dtype=torch.complex64)
+    b = torch.rand(NUM_OF_LATTICES, *LATTICE, GAUGE_DOF, NON_GAUGE_DOF, dtype=torch.complex64)
 
     time_before = perf_counter_ns()
-    field, res, steps, res_list = gmres(d_wc, b, preconditioner, preconditioner(b), 1000, 0.01)
+    field, res, steps, res_list = gmres(d_wc, b, preconditioner, preconditioner(b), 1000, 0.01, True)
     time_taken = (perf_counter_ns() - time_before) * 1e-6
     error = torch.norm((d_wc(field) - b).view(-1))
+    res_list.append(time_taken)
     res_list.append(error)
 
     torch.save(field, "hpd_test/" + tra_name + ".pt")
