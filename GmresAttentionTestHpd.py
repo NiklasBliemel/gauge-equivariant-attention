e029544f875cf238105e035979bfe2ecaba3d2a3
Niklas Bliemel <niklas.bliemel@stud.uni-regensburg.de>
2024-05-19 11:45:25 +0200

    hpd_test

--- GmresAttentionTestHpd.py
+++ GmresAttentionTestHpd.py
@@ -1,11 +1,22 @@
-from GMRES import GmresTest
+from GMRES import gmres
 from Operators import D_WC
 from Constants import *
-from Preconditioner import *
+from Preconditioner import transformer, ptc
+import pickle
+from time import perf_counter_ns
 
-d_wc = D_WC(M, GAUGE_FIELD)
+d_wc = D_WC(M, GAUGE_FIELD_SMALL)
 
-gmres_test = GmresTest(d_wc, NUM_OF_LATTICES, LATTICE, GAUGE_DOF, NON_GAUGE_DOF, max_iter=100, tol=1e-1)
+Preconditioner = transformer("tr_4_16_True")
 
-Tr = transformer("Tr_4_16_True")
-gmres_test(Tr)
+b = torch.rand(NUM_OF_LATTICES, *LATTICE_SMALL, GAUGE_DOF, NON_GAUGE_DOF, dtype=torch.complex64)
+
+time_before = perf_counter_ns()
+field, res, steps, res_list = gmres(d_wc, b, Preconditioner, Preconditioner(b), 1000, 0.01, True)
+time_taken = (perf_counter_ns() - time_before) * 1e-6
+error = torch.norm((d_wc(field) - b).view(-1))
+
+torch.save(field, "hpd_test/solution_tra_0.pt")
+
+with open('hpd_test/res_list_tra_0.pkl', 'wb') as f:
+    pickle.dump(res_list, f)
