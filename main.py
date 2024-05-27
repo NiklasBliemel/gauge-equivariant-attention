b7d2f8f3e8d11fb3bc8e95823af790711499a6be
Niklas Bliemel <niklas.bliemel@stud.uni-regensburg.de>
2024-05-27 18:51:21 +0200

    New Data

--- main.py
+++ main.py
@@ -11,7 +11,20 @@ Functions that are designed for main:
 * gmres_test
 """""
 
-print("Starting Transformer training!")
+torch.cuda.empty_cache()
+print("Starting PTC training!")
+time_point = time()
+train_module(PTC, structure=(NON_GAUGE_DOF, NON_GAUGE_DOF, PTC_PATHES, GAUGE_FIELD_SMALL), small=True)
+print(f"Finished PTC training in {(time() - time_point):.1f} s")
+
+torch.cuda.empty_cache()
+print("Starting SuperPtc training!")
+time_point = time()
+train_module(SuperPtc, structure=(GAUGE_FIELD_SMALL, NON_GAUGE_DOF), small=True)
+print(f"Finished SuperPtc training in {(time() - time_point):.1f} s")
+
+torch.cuda.empty_cache()
+print("Starting Transformertraining!")
 time_point = time()
 train_module(Transformer, structure=(GAUGE_FIELD_SMALL, NON_GAUGE_DOF, 16), small=True)
-print(f"Finished Transformer training in {(time() - time_point):.1f} ms")
+print(f"Finished Transformer training in {(time() - time_point):.1f} s")
